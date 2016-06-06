c238b4e03edbfcf79b41de6b52f19524ed1df5f8
Luiz Felippe <lf.lazzarin@gmail.com>
2016-06-06 02:49:36 -0300

    Correction on the Forecast Columns indexes

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -76,8 +76,8 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     static final int COL_WEATHER_DESC = 2;
     static final int COL_WEATHER_MAX_TEMP = 3;
     static final int COL_WEATHER_MIN_TEMP = 4;
-    static final int COL_LOCATION_SETTING = 5;
-    static final int COL_WEATHER_CONDITION_ID = 6;
+    static final int COL_WEATHER_CONDITION_ID = 5;
+    static final int COL_LOCATION_SETTING = 6;
     static final int COL_COORD_LAT = 7;
     static final int COL_COORD_LONG = 8;
 
